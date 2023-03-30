c2fb200c8f84f60b576f05f812eebebf898e5bde
suryawanshishreyas <suryawanshishreyas@gmail.com>
2023-03-30 14:24:53 +0530

    Making-a-Chrome-Extension

---
+++ block-BJaaez/code/data.js
@@ -0,0 +1,490 @@
+let data=[
+        {
+            "shortcut": "",
+            "description": "Quick Open"
+        },
+        {
+            "shortcut": "",
+            "description": "New window/instance"
+        },
+        {
+            "shortcut": "",
+            "description": "Close window/instance"
+        },
+        {
+            "shortcut": "",
+            "description": "Cut line (empty selection)"
+        },
+        {
+            "shortcut": "",
+            "description": "Copy line (empty selection)"
+        },
+        {
+            "shortcut": "",
+            "description": "Move line up/down"
+        },
+        {
+            "shortcut": "",
+            "description": "Copy line up/down"
+        },
+        {
+            "shortcut": "",
+            "description": "Delete line"
+        },
+        {
+            "shortcut": "",
+            "description": "Insert line below"
+        },
+        {
+            "shortcut": "",
+            "description": "Insert line above"
+        },
+        {
+            "shortcut": "",
+            "description": "Jump to matching bracket"
+        },
+        {
+            "shortcut": "",
+            "description": "Indent/outdent line"
+        },
+        {
+            "shortcut": "",
+            "description": "Go to beginning of line"
+        },
+        {
+            "shortcut": "",
+            "description": "Go to End of line"
+        },
+        {
+            "shortcut": "",
+            "description": "Go to beginning of file"
+        },
+        {
+            "shortcut": "",
+            "description": "Go to End of file"
+        },
+        {
+            "shortcut": "",
+            "description": "Scroll line up/down"
+        },
+        {
+            "shortcut": "",
+            "description": "Scroll page up/down"
+        },
+        {
+            "shortcut": "",
+            "description": "Fold (collapse) region"
+        },
+        {
+            "shortcut": "",
+            "description": "Unfold (uncollapse) region"
+        },
+        {
+            "shortcut": "",
+            "description": "Fold (collapse) all subregions"
+        },
+        {
+            "shortcut": "",
+            "description": "Unfold (uncollapse) all subregions"
+        },
+        {
+            "shortcut": "",
+            "description": "Fold (collapse) all regions"
+        },
+        {
+            "shortcut": "",
+            "description": "Unfold (uncollapse) all regions"
+        },
+        {
+            "shortcut": "",
+            "description": "Add line comment"
+        },
+        {
+            "shortcut": "",
+            "description": "Remove line comment"
+        },
+        {
+            "shortcut": "",
+            "description": "Change theme"
+        },
+        {
+            "shortcut": "",
+            "description": "Toggle line comment"
+        },
+        {
+            "shortcut": "",
+            "description": "Toggle block comment"
+        },
+        {
+            "shortcut": "",
+            "description": "Toggle word wrap"
+        },
+        {
+            "shortcut": "",
+            "description": "Show all Symbols"
+        },
+        {
+            "shortcut": "",
+            "description": "Go to Line..."
+        },
+        {
+            "shortcut": "",
+            "description": "Go to File..."
+        },
+        {
+            "shortcut": "",
+            "description": "Go to Symbol..."
+        },
+        {
+            "shortcut": "",
+            "description": "Show Problems panel"
+        },
+        {
+            "shortcut": "",
+            "description": "Go to next error or warning"
+        },
+        {
+            "shortcut": "",
+            "description": "Go to previous error or warning"
+        },
+        {
+            "shortcut": "",
+            "description": "Navigate editor group history"
+        },
+        {
+            "shortcut": "",
+            "description": "Go back / forward"
+        },
+        {
+            "shortcut": "",
+            "description": "Toggle Tab moves focus"
+        },
+        {
+            "shortcut": "",
+            "description": "Find"
+        },
+        {
+            "shortcut": "",
+            "description": "Replace"
+        },
+        {
+            "shortcut": "",
+            "description": "Find next/previous"
+        },
+        {
+            "shortcut": "",
+            "description": "Select all occurences of Find match"
+        },
+        {
+            "shortcut": "",
+            "description": "Add selection to next Find match"
+        },
+        {
+            "shortcut": "",
+            "description": "Move last selection to next Find match"
+        },
+        {
+            "shortcut": "",
+            "description": "Toggle Case-sensitive/Regex/Whole word"
+        },
+        {
+            "shortcut": "",
+            "description": "Insert cursor"
+        },
+        {
+            "shortcut": "",
+            "description": "Insert cursor above / below"
+        },
+        {
+            "shortcut": "",
+            "description": "Undo last cursor operation"
+        },
+        {
+            "shortcut": "",
+            "description": "Insert cursor at end of each line selected"
+        },
+        {
+            "shortcut": "",
+            "description": "Select current line"
+        },
+        {
+            "shortcut": "",
+            "description": "Select all occurrences of current selection"
+        },
+        {
+            "shortcut": "",
+            "description": "Select all occurrences of current word"
+        },
+        {
+            "shortcut": "",
+            "description": "Expand selection"
+        },
+        {
+            "shortcut": "",
+            "description": "Shrink selection"
+        },
+        {
+            "shortcut": "",
+            "description": "Column (box) selection"
+        },
+        {
+            "shortcut": "",
+            "description": "Column (box) selection"
+        },
+        {
+            "shortcut": "",
+            "description": "Column (box) selection page up/down"
+        },
+        {
+            "shortcut": "",
+            "description": "Trigger suggestion"
+        },
+        {
+            "shortcut": "",
+            "description": "Trigger parameter hints"
+        },
+        {
+            "shortcut": "",
+            "description": "Emmet expand abbreviation"
+        },
+        {
+            "shortcut": "",
+            "description": "Format document"
+        },
+        {
+            "shortcut": "",
+            "description": "Format selection"
+        },
+        {
+            "shortcut": "",
+            "description": "Go to Definition"
+        },
+        {
+            "shortcut": "",
+            "description": "Peek Definition"
+        },
+        {
+            "shortcut": "",
+            "description": "Open Definition to the side"
+        },
+        {
+            "shortcut": "",
+            "description": "Quick Fix"
+        },
+        {
+            "shortcut": "",
+            "description": "Show References"
+        },
+        {
+            "shortcut": "",
+            "description": "Rename Symbol"
+        },
+        {
+            "shortcut": "",
+            "description": "Replace with next/previous value"
+        },
+        {
+            "shortcut": "",
+            "description": "Trim trailing whitespace"
+        },
+        {
+            "shortcut": "",
+            "description": "Change file language"
+        },
+        {
+            "shortcut": "",
+            "description": "Close editor"
+        },
+        {
+            "shortcut": "",
+            "description": "Close folder"
+        },
+        {
+            "shortcut": "",
+            "description": "Split editor"
+        },
+        {
+            "shortcut": "",
+            "description": "Focus into 1st, 2nd or 3rd editor group"
+        },
+        {
+            "shortcut": "",
+            "description": "Focus into previous/next editor group"
+        },
+        {
+            "shortcut": "",
+            "description": "Move editor left/right"
+        },
+        {
+            "shortcut": "",
+            "description": "Move active editor group"
+        },
+        {
+            "shortcut": "",
+            "description": "New File"
+        },
+        {
+            "shortcut": "",
+            "description": "Open File"
+        },
+        {
+            "shortcut": "",
+            "description": "Save"
+        },
+        {
+            "shortcut": "",
+            "description": "Save As"
+        },
+        {
+            "shortcut": "",
+            "description": "Save All"
+        },
+        {
+            "shortcut": "",
+            "description": "Close"
+        },
+        {
+            "shortcut": "",
+            "description": "Close All"
+        },
+        {
+            "shortcut": "",
+            "description": "Reopen closed editor"
+        },
+        {
+            "shortcut": "",
+            "description": "Keep Open"
+        },
+        {
+            "shortcut": "",
+            "description": "Open next"
+        },
+        {
+            "shortcut": "",
+            "description": "Open previous"
+        },
+        {
+            "shortcut": "",
+            "description": "Copy Path of active file"
+        },
+        {
+            "shortcut": "",
+            "description": "Reveal active file in Explorer"
+        },
+        {
+            "shortcut": "",
+            "description": "Show active file in new window/instance"
+        },
+        {
+            "shortcut": "",
+            "description": "Toggle full screen"
+        },
+        {
+            "shortcut": "",
+            "description": "Toggle editor layout"
+        },
+        {
+            "shortcut": "",
+            "description": "Zoom in/out"
+        },
+        {
+            "shortcut": "",
+            "description": "Toggle Sidebar visibility"
+        },
+        {
+            "shortcut": "",
+            "description": "Show Explorer / Toggle focus"
+        },
+        {
+            "shortcut": "",
+            "description": "Show Search"
+        },
+        {
+            "shortcut": "",
+            "description": "Show Git"
+        },
+        {
+            "shortcut": "",
+            "description": "Show Debug"
+        },
+        {
+            "shortcut": "",
+            "description": "Show Extensions"
+        },
+        {
+            "shortcut": "",
+            "description": "Replace in files"
+        },
+        {
+            "shortcut": "",
+            "description": "Toggle Search details"
+        },
+        {
+            "shortcut": "",
+            "description": "Open new command prompt/terminal"
+        },
+        {
+            "shortcut": "",
+            "description": "Show Output panel"
+        },
+        {
+            "shortcut": "",
+            "description": "Toggle Markdown preview"
+        },
+        {
+            "shortcut": "",
+            "description": "Open Markdown preview to the side"
+        },
+        {
+            "shortcut": "",
+            "description": "Toggle breakpoint"
+        },
+        {
+            "shortcut": "",
+            "description": "Start/Continue"
+        },
+        {
+            "shortcut": "",
+            "description": "Stop"
+        },
+        {
+            "shortcut": "",
+            "description": "Step into/out"
+        },
+        {
+            "shortcut": "",
+            "description": "Step over"
+        },
+        {
+            "shortcut": "",
+            "description": "Show hover"
+        },
+        {
+            "shortcut": "",
+            "description": "Show integrated terminal"
+        },
+        {
+            "shortcut": "",
+            "description": "Create new terminal"
+        },
+        {
+            "shortcut": "",
+            "description": "Copy selection"
+        },
+        {
+            "shortcut": "",
+            "description": "Paste into active terminal"
+        },
+        {
+            "shortcut": "",
+            "description": "Scroll up/down"
+        },
+        {
+            "shortcut": "",
+            "description": "Scroll page up/down"
+        },
+        {
+            "shortcut": "",
+            "description": "Scroll to top/bottom"
+        }
+]
